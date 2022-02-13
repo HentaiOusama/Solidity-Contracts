b3fc8567b7e8f364a663cd3b18cc4c7a38d8eefc
Hentai Ousama <kutabare.eerito@gmail.com>
2022-02-14 02:41:53 +0530

    Smal QoL change.

--- TIKI Token/Latest/Tiki.sol
+++ TIKI Token/Latest/Tiki.sol
@@ -1222,21 +1222,14 @@ contract TIKIDividendTracker is DividendPayingToken, Ownable {
     if (numberOfTokenHolders == 0) { return (0, 0, lastProcessedIndex); }
 
     uint256 _lastProcessedIndex = lastProcessedIndex;
-
     uint256 gasUsed = 0;
-
     uint256 gasLeft = gasleft();
-
     uint256 iterations = 0;
     uint256 claims = 0;
 
     while (gasUsed < gas && iterations < numberOfTokenHolders) {
       _lastProcessedIndex++;
-
-      if (_lastProcessedIndex >= tokenHoldersMap.keys.length) {
-        _lastProcessedIndex = 0;
-      }
-
+      if (_lastProcessedIndex >= tokenHoldersMap.keys.length) { _lastProcessedIndex = 0; }
       address account = tokenHoldersMap.keys[_lastProcessedIndex];
 
       if (canAutoClaim(lastClaimTimes[account]) && processAccount(payable(account), true)) {
